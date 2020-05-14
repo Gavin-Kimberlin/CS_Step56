ad8fae022f78c69228b8e6016a057db4b9f86926
Gavin Kimberlin <guviner@gmail.com>
2020-05-13 19:07:33 -0700

    Create a basic program that prints text to the console.

--- CS_Step56/Program.cs
+++ CS_Step56/Program.cs
@@ -1,15 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CS_Step56
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, we’re “accounting” on you!");
+            Console.Read();
         }
     }
 }
